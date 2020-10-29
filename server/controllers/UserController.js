786c26c1f5a37e1fbf94cc407ae641cac33df3a3
Gerry Anthony <gerrysimangunsong@gmail.com>
2020-10-29 13:51:27 +0700

    1. add function signIn to UserController 2. add POST /users/sign-in to userRouter

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -17,6 +17,35 @@ class UserController {
       next(err);      
     }
   }
+
+  static async signIn(req, res, next) {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({
+        where: {
+          email
+        }
+      });
+
+      if (!user) {
+        throw {
+          name: 'InvalidUserPassword'
+        }
+      } else if (!comparePassword(password, user.password)) {
+        throw {
+          name: 'InvalidUserPassword'
+        }
+      } else {
+        const accessToken = signInToken({
+          id: user.id,
+          email
+        });
+        res.status(200).json({ accessToken });
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 module.exports = UserController;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const UserController = require('../controllers/UserController');
 
 router.post('/sign-up', UserController.signUp);
+router.post('/sign-in', UserController.signIn);
 
 module.exports = router;
