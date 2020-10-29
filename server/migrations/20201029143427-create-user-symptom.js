adbc7f30d0af9c66690250780b5e5f8940bb15b1
Gerry Anthony <gerrysimangunsong@gmail.com>
2020-10-29 21:45:42 +0700

    1. add model Symptom and usersymptom 2. add association between models

---
+++ server/migrations/20201029143427-create-user-symptom.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('UserSymptoms', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+      SymptomId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Symptoms',
+          key: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('UserSymptoms');
+  }
+};
