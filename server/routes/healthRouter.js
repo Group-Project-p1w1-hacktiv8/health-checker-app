8cfd3d6549889be489a1451bf6a41d4b2e83b2ad
Gerry Anthony <gerrysimangunsong@gmail.com>
2020-10-29 23:35:01 +0700

    1. add diagnosis to HealthController 2. add GET /issues to healthRouter

--- server/routes/healthRouter.js
+++ server/routes/healthRouter.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const HealthController = require('../controllers/HealthController');
 
 router.get('/symptoms', HealthController.findSymptoms);
+
+router.get('/issues', HealthController.diagnosis);
+
+// ! need req.body { issueId }
 router.post('/treatment', HealthController.findTreatment);
 
 module.exports = router;
