411aba53f0a04d0593e185380c3ae15fe1d6918d
erick <erick.gunawan70@gmail.com>
2020-10-29 19:49:59 +0700

    1. added classes to sign in and sign up 2. edited main.js

--- client/main.js
+++ client/main.js
@@ -1,8 +1,46 @@
+const server = "http//localhost:3000"
+
 function signIn(e){
     e.preventDefault()
     // console.log("button terclick")
     const email = $("#email").val()
     const password = $("#email").val()
-    const age = $(new Date() - Date("#birth_date"))
-    const name = $("#first_name" + " " + "#last_name")
+    $.ajax({
+        method: "POST",
+        url: server + "/sign-in",
+        data: {
+            email,
+            password
+        }
+    }).done(response =>{
+        console.log(response)
+    }).fail(err => {
+        console.log(err)
+    })
+}
+
+function signUp(e){
+    e.preventDefault()
+    const email = $("#email").val()
+    const password = $("#email").val()
+    const first_name = $("#first_name").val()
+    const last_name = $("#last_name").val()
+    const birth_year = $("#birth_date").val()
+    const gender = $("#gender").val()
+    $.ajax({
+        method: "POST",
+        url: server + "/sign-up",
+        data: {
+            email,
+            password,
+            first_name,
+            last_name,
+            birth_year,
+            gender
+        }
+    }).done(response =>{
+        console.log(response)
+    }).fail(err => {
+        console.log(err)
+    })
 }
