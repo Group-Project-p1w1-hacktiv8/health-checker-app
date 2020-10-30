1d04992220089d96ad2ac30cdedc73e6f1e0876e
Gerry Anthony <gerrysimangunsong@gmail.com>
2020-10-30 14:12:46 +0700

    resolve

--- client/main.js
+++ client/main.js
@@ -255,7 +255,7 @@ function getTreatment(e, issueId) {
 
 function removeButton() {
   $('#buttonIssue').remove();
-=======
+}
 function onSignIn(googleUser) {
     // var profile = googleUser.getBasicProfile();
     // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
